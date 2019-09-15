430f77b7ad6dd7b270d54d85ee2d53e3c57b1203
João Pedro Lopes Mendes <joaopedrolopesmendes>
2019-09-15 04:04:42 +0100

    Added button to copy to clipboard and to remove

--- public/js/files/main.js
+++ public/js/files/main.js
@@ -2,12 +2,16 @@
 $(window).on("load", () => {
 
     function getImagesData() {
+        // language=HTML
         const rowTemplate = `<div class="grid-item shadow-lg position-relative" id="%id%">
                                 <img src="%img_src%" class="img-fluid">
                                 <div class="group-buttons">
-                                    <a class="btn custom-btn btn-info clipboard">X</a>
-                                    <a href="%delete_route%/%id%" class="btn custom-btn btn-danger">X</a>
-
+                                    <a class="btn custom-btn btn-info clipboard" data-toggle="tooltip" data-placement="right" title="Copy url to the clipboard">
+                                        <i class="far fa-clipboard"></i>
+                                    </a>
+                                    <form method="POST" action="%delete_route%"  accept-charset="UTF-8" class="d-inline"><input name="_method" type="hidden" value="DELETE"><input name="_token" type="hidden" value=%csrf%>
+                                    <span class="waves-input-wrapper waves-effect waves-light"><input class="btn custom-btn btn-danger" type="submit" value="X" data-toggle="tooltip" data-placement="right" title="Delete the image"></span>
+                                </form>
                                 </div>
                              </div>`;
         $.getJSON(window.getAllFilesUrl)
@@ -17,7 +21,7 @@ $(window).on("load", () => {
                     template = template
                         .replace(/%id%/g, file.id)
                         .replace(/%name%/g, file.name)
-                        .replace(/%delete_route%/g, $('meta[name="APP_URL"]').prop('content') + '/backoffice/file/delete/' + file.path)
+                        .replace(/%delete_route%/g, $('meta[name="APP_URL"]').prop('content') + '/backoffice/file/' + file.id)
                         .replace(/%csrf%/g, $('meta[name="csrf_token"]').prop('content'))
                         .replace(/%img_src%/g, $('meta[name="APP_URL"]').prop('content') + '/storage/' + file.path);
 
@@ -32,5 +36,6 @@ $(window).on("load", () => {
 
     getImagesData();
 
+    
 
 });
