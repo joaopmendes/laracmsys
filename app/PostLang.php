e3b0c9157cefd5c5c2f54fc87c1dd27b9b0409dc
João Pedro Lopes Mendes <joaopedrolopesmendes>
2019-09-16 23:19:53 +0100

    deleted template

--- app/PostLang.php
+++ app/PostLang.php
@@ -4,18 +4,6 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class TagLang extends Model
-{
-   protected $table = 'language_tag';
-   protected $fillable = ["tag_id", "language_id", "name"];
-}
-
-<?php
-
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
 class PostLang extends Model
 {
    protected $table = 'language_post';
