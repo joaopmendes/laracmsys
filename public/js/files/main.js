5a9543b17d8852108bd23abf6271700797bb4d8d
João Pedro Lopes Mendes <joaopedrolopesmendes>
2019-09-16 23:38:04 +0100

    Clipboard added

--- public/js/files/main.js
+++ public/js/files/main.js
@@ -6,7 +6,7 @@ $(window).on("load", () => {
         const rowTemplate = `<div class="grid-item shadow-lg position-relative" id="%id%">
                                 <img src="%img_src%" class="img-fluid">
                                 <div class="group-buttons">
-                                    <a class="btn custom-btn btn-info clipboard" data-toggle="tooltip" data-placement="right" title="Copy url to the clipboard">
+                                    <a class="btn custom-btn btn-info clipboard" data-toggle="tooltip" data-url="%img_src%" data-placement="right" title="Copy url to the clipboard">
                                         <i class="far fa-clipboard"></i>
                                     </a>
                                     <form method="POST" action="%delete_route%"  accept-charset="UTF-8" class="d-inline"><input name="_method" type="hidden" value="DELETE"><input name="_token" type="hidden" value=%csrf%>
@@ -29,7 +29,9 @@ $(window).on("load", () => {
                 });
                 $(".clipboard").on('click', function (event) {
                     event.preventDefault();
-                    navigator.clipboard.writeText(event.target.src).then(r => {});
+                    navigator.clipboard.writeText($(event.target).data('url')).then(r => {
+                        alert('Link Copied.')
+                    });
                 });
             })
     }
